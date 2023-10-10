1c94bb31bb345a004ba4239124d8edd95f850311
Sebastian Liu <sebsadface@gmail.com>
2023-10-09 19:18:36 -0700

    rmove dslabs

--- dslabs/labs/lab1-clientserver/src/dslabs/atmostonce/AMOCommand.java
+++
@@ -1,9 +0,0 @@
-package dslabs.atmostonce;
-
-import dslabs.framework.Command;
-import lombok.Data;
-
-@Data
-public final class AMOCommand implements Command {
-    // Your code here...
-}
--- dslabs/labs/lab1-clientserver/src/dslabs/atmostonce/AMOResult.java
+++
@@ -1,9 +0,0 @@
-package dslabs.atmostonce;
-
-import dslabs.framework.Result;
-import lombok.Data;
-
-@Data
-public final class AMOResult implements Result {
-    // Your code here...
-}
